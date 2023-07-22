2ef7ee8434b91850f0de6d74804214cfac5295f3
Colin Spangelo <colin.spangelo@gmail.com>
2023-07-22 15:26:53 -0600

    Fixed the error not hiding on submission with the help of Rohit.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,74 +5,68 @@
  */
 // const tweetObject = require('../../server/data-files/initial-tweets.json');
 
-const tweetArray = [];
+$('#document').ready(function(e){
+  const tweetArray = [];
 
-const createTweetElement = function(tweetObject) {
-  const $tweet = $(
-    `<article>
-    <header class="sideBySide center containerMargin">
-      <div class="sideBySide center">
-        <div><img src="${tweetObject.user.avatars}"></div>
-        <div>${tweetObject.user.name}</div>
+  const createTweetElement = function(tweetObject) {
+    const $tweet = $(
+      `<article>
+      <header class="sideBySide center containerMargin">
+        <div class="sideBySide center">
+          <div><img src="${tweetObject.user.avatars}"></div>
+          <div>${tweetObject.user.name}</div>
+        </div>
+        <div>${tweetObject.user.handle}</div>
+      </header>
+      <div class="tweetPadding bold bottomBorder">
+      <p>${escape(tweetObject.content.text)}</p>
       </div>
-      <div>${tweetObject.user.handle}</div>
-    </header>
-    <div class="tweetPadding bold bottomBorder">
-    <p>${escape(tweetObject.content.text)}</p>
-    </div>
-    <footer class="sideBySide makeSmaller containerMargin">
-      <div class="bold">${timeago.format(tweetObject.created_at)}</div>
-      <div class="sideBySide splitUp ">
-        <div><i class="fa-solid fa-flag icon"></i></div>
-        <div><i class="fa-solid fa-retweet icon"></i></div>
-        <div><i class="fa-solid fa-heart icon"></i></div>
-      </div>
-    </footer>
-  </article>`
-  );
-  return $tweet;
-};
-//Anti hacking feature
-const escape = function (str) {
-  let div = document.createElement("div");
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-};
-
-const hideFunc =function(){
-  $(document).ready(function() {
-    $("#emptyError").addClass("hide");
-});
-$(document).ready(function() {
-  $("#toLongError").addClass("hide");
-});
-  
-}
-
-
-$(document).ready(function() {
-  //Creates the tweets in the tweet array and then renders the tweets in the tweet container
-  const renderTweets = function(tweetArray) {
-    for (const tweets of tweetArray) {
-      const $tweet = createTweetElement(tweets);
-      $('#tweets-container').append($tweet);
-    }
+      <footer class="sideBySide makeSmaller containerMargin">
+        <div class="bold">${timeago.format(tweetObject.created_at)}</div>
+        <div class="sideBySide splitUp ">
+          <div><i class="fa-solid fa-flag icon"></i></div>
+          <div><i class="fa-solid fa-retweet icon"></i></div>
+          <div><i class="fa-solid fa-heart icon"></i></div>
+        </div>
+      </footer>
+    </article>`
+    );
+    return $tweet;
   };
 
+  //Anti hacking feature
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
-  const loadTweets = function() {
-    $.ajax("/tweets", { method: "GET" })
-      .then(function(data, status) {
-        // console.log(tweetsObject[data])
-        for (const info of data) {
-          tweetArray.push(info);
-        }
-        renderTweets(tweetArray);
+  const hideFunc =function(){
+    $("#emptyError").hide();
+    $("#toLongError").hide();
+  }
+
+    //Creates the tweets in the tweet array and then renders the tweets in the tweet container
+    const renderTweets = function(tweetArray) {
+      for (const tweets of tweetArray) {
+        const $tweet = createTweetElement(tweets);
+        $('#tweets-container').append($tweet);
       }
-      );
-  };
+    };
 
-  // Event handler on the submit button
+    const loadTweets = function() {
+      $.ajax("/tweets", { method: "GET" })
+        .then(function(data, status) {
+          // console.log(tweetsObject[data])
+          for (const info of data) {
+            tweetArray.push(info);
+          }
+          renderTweets(tweetArray);
+        }
+        );
+    };
+
+    // Event handler on the submit button
 
   $("form").submit(function(event) {
     // const text = $("#tweet-text").serialize();
@@ -99,7 +93,5 @@ $(document).ready(function() {
       loadTweets();
       hideFunc();
     });
-
   });
-});
-
+}); //Document ready finishes here.
