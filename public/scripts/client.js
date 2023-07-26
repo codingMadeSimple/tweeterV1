6a34fad9b3da2e365159a95b59aefcccda14e515
Colin Spangelo <colin.spangelo@gmail.com>
2023-07-26 15:41:07 -0600

    Worked through with Andy the double posting issue, wordwrap issue, applied the catch block properly, and triming before checking the text value.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,7 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$('#document').ready(function(e){
+$('#document').ready(function(e) {
   const tweetArray = [];
 
   const createTweetElement = function(tweetObject) {
@@ -18,7 +18,7 @@ $('#document').ready(function(e){
         <div>${tweetObject.user.handle}</div>
       </header>
       <div class="tweetPadding bold bottomBorder">
-      <p>${escape(tweetObject.content.text)}</p>
+      <p class="tweet-content"">${escape(tweetObject.content.text)}</p>
       </div>
       <footer class="sideBySide makeSmaller">
         <div class="bold">${timeago.format(tweetObject.created_at)}</div>
@@ -34,60 +34,56 @@ $('#document').ready(function(e){
   };
 
   //Anti hacking feature
-  const escape = function (str) {
+  const escape = function(str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
-  const hideFunc =function(){
+  const hideFunc = function() {
     $("#emptyError").hide();
     $("#toLongError").hide();
-  }
+  };
 
-    //Creates the tweets in the tweet array and then renders the tweets in the tweet container
-    const renderTweets = function(tweets) {
-      // Clear tweat-container first
-      $("#tweets-container").empty();
-      for (const data of tweets) {
-        const $tweet = createTweetElement(data);
-        $('#tweets-container').prepend($tweet);
+  //Creates the tweets in the tweet array and then renders the tweets in the tweet container
+  const renderTweets = function(tweets) {
+    // Clear tweat-container first
+    $("#tweets-container").empty();
+    for (const data of tweets) {
+      const $tweet = createTweetElement(data);
+      $('#tweets-container').prepend($tweet);
+    }
+  };
+  // renderTweets();
+
+
+  const loadTweets = function() {
+    $.ajax("/tweets", { method: "GET" })
+      .then(function(data, status) {
+        renderTweets(data);
       }
-    };
-    // renderTweets();
+      ).catch((error) => {
+        console.log("An error has occured.")
+      })
+  };
 
-
-    const loadTweets = function() {
-      $.ajax("/tweets", { method: "GET" })
-        .then(function(data, status) {
-          // $('#tweets-container').prepend($tweet);
-          // for (const info of data) {
-          //   tweetArray.push(info);
-          // }
-          renderTweets(data);
-        }
-        );
-    };
-    loadTweets();
-    // Event handler on the submit button
+  loadTweets();
+  // Event handler on the submit button
 
   $("form").submit(function(event) {
-    const text = document.getElementById("tweet-text").value;
-    
+    const text = $("#tweet-text").val().trim();
     event.preventDefault();
+
+    //Should these be in the ajax request
     if (text.length === 0) {
-      event.preventDefault();
-      return $("#emptyError").slideDown()
+      return $("#emptyError").slideDown();
     } else if (text.length > 140) {
-      event.preventDefault();
-      return $("#toLongError").slideDown()
-    } else if (text === null) {
-      alert("Please input something")
-      event.preventDefault();
-    } else if(text.trim.length === 0){
-      alert("Please input something other than spaces")
-      event.preventDefault();
+      return $("#toLongError").slideDown();
     }
+
+
+
+
     //Ajax post request to post data from the textbox and the submit button
     $.ajax({
       method: 'POST',
@@ -95,13 +91,14 @@ $('#document').ready(function(e){
       data: $(this).serialize()
     }).then((response) => {
       console.log('inside the .then', response);
+
+      //Call create tweet function here 
       loadTweets();
       hideFunc();
 
-
       //Something is going wrong here
-    }).catch(
-      send
-    );
+    }).catch((error) =>{
+      console.log(error)
+    })
   });
 }); //Document ready finishes here.
